e6cae79e116e2a01397c86a9a34263326724457f
José Moreno Rodríguez <35952698+JoseMoreno95@users.noreply.github.com>
2018-04-12 15:44:56 +0200

    Añadida funcionalidad del PUT.

--- contentapp.py
+++ contentapp.py
@@ -1,50 +1,38 @@
 #!/usr/bin/python3
 
-"""
- contentApp class
- Simple web application for managing content
-
- Copyright Jesus M. Gonzalez-Barahona, Gregorio Robles 2009-2015
- jgb, grex @ gsyc.es
- TSAI, SAT and SARO subjects (Universidad Rey Juan Carlos)
- October 2009 - March 2015
-"""
-
 import webapp
 
+class contentPutApp (webapp.webApp):
 
-class contentApp (webapp.webApp):
-    """Simple web application for managing content.
-
-    Content is stored in a dictionary, which is intialized
-    with the web content."""
-
-    # Declare and initialize content
     content = {'/': 'Root page',
                '/page': 'A page'
                }
 
     def parse(self, request):
-        """Return the resource name (including /)"""
+        method = request.split(' ')[0]
+        resource = request.split(' ')[1]
+        try:
+            body = request.split('\r\n\r\n')[1]
+        except IndexError:
+            body = None
+        return (method, resource, body)
 
-        return request.split(' ', 2)[1]
-
-    def process(self, resourceName):
-        """Process the relevant elements of the request.
-
-        Finds the HTML text corresponding to the resource name,
-        ignoring requests for resources not in the dictionary.
-        """
-
-        if resourceName in self.content.keys():
-            httpCode = "200 OK"
-            htmlBody = "<html><body>" + self.content[resourceName] \
-                + "</body></html>"
+    def process(self, splittedRequest):
+        if splittedRequest[0] == 'GET':
+            if splittedRequest[1] in self.content.keys():
+                httpCode = "200 OK"
+                htmlBody = "<html><body>" + self.content[splittedRequest[1]] \
+                    + "</body></html>"
+            else:
+                httpCode = "404 Not Found"
+                htmlBody = "Not Found"
+            return (httpCode, htmlBody)
         else:
-            httpCode = "404 Not Found"
-            htmlBody = "Not Found"
-        return (httpCode, htmlBody)
-
+            self.content[splittedRequest[1]] = splittedRequest[2]
+            httpCode = "200 OK"
+            htmlBody = "<html><body>" + self.content[splittedRequest[1]] \
+                + "</body></html>"
+            return (httpCode, htmlBody)
 
 if __name__ == "__main__":
-    testWebApp = contentApp("localhost", 1234)
+    testWebApp = contentPutApp("localhost", 1234)
